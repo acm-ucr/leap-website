261fba60c7e88b9d245e50408c0ab5360d1fdd06
rishikpulipaka <rpuli007@ucr.edu>
2025-01-14 21:15:17 -0800

    fixed sizing issue on event cards, works as intended

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -9,15 +9,15 @@ interface EventProps {
 
 const Events: React.FC<EventProps> = ({ date, time, title, desc }) => {
   return (
-    <div className="flex h-[1/3vh] w-[1050px] items-center overflow-hidden rounded-lg">
-      <div className="h-full bg-[#2A7860] p-7">
+    <div className="grid h-[1/3vh] w-[1050px] grid-cols-7 items-center overflow-hidden rounded-lg">
+      <div className="col-span-1 flex h-full items-center justify-center bg-[#2A7860]">
         <div className="flex flex-col items-center justify-center px-10 py-7 text-center text-white">
           <div className="text-3xl font-semibold">{date}</div>
           <div className="text-sm">{time}</div>
         </div>
       </div>
-      <div className="h-full] bg-[#f7eee3] p-7">
-        <div className="">
+      <div className="col-span-6 h-full bg-[#f7eee3]">
+        <div className="p-7">
           <h1 className="text-4xl font-semibold text-[#203e4e]">{title}</h1>
           <p className="mt-2 text-[#203e4e]">{desc}</p>
         </div>
