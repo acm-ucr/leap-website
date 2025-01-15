3d7e7859051789c151beccd04819d1f594cf6b02
rishikpulipaka <rpuli007@ucr.edu>
2025-01-14 19:36:17 -0800

    Event Card

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,8 +2,32 @@ import Events from "../../components/events/Events";
 
 const OurEvents = () => {
   return (
-    <div>
-      <Events />
+    <div className="flex flex-col items-center justify-center gap-4 p-8">
+      <Events
+        date="FEB 29"
+        time="7:00PM"
+        title="Life in the Industry: PM"
+        desc="
+          For our second installment of the Life in the Industry series: Learn all about the role as a Product Manager, from the details of the day-to-day work schedule to the ranking status, salaries, and the people you work with in this position.
+          "
+      />
+      <Events
+        date="FEB 29"
+        time="7:00PM"
+        title="Life in the Industry: PM"
+        desc="
+          For our second installment of the Life in the Industry series: Learn all about the role as a Product Manager, from the details of the day-to-day work schedule to the ranking status, salaries, and the people you work with in this position.
+          For our second installment of the Life in the Industry series: Learn all about the role as a Product Manager, from the details of the day-to-day work schedule to the ranking status, salaries, and the people you work with in this position.
+          "
+      />
+      <Events
+        date="FEB 29"
+        time="7:00PM"
+        title="Life in the Industry: PM"
+        desc="
+          For our second installment of the Life in the Industry series: Learn all about the role as a Product Manager, from the details of the day-to-day work schedule to the ranking status, salaries, and the people you work with in this position.
+          "
+      />
     </div>
   );
 };
--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const Events = () => {
-  return <div>Events Page</div>;
+interface EventProps {
+  date: string;
+  time: string;
+  title: string;
+  desc: string;
+}
+
+const Events: React.FC<EventProps> = ({ date, time, title, desc }) => {
+  return (
+    <div className="flex h-[1/3vh] w-[1050px] items-center overflow-hidden rounded-lg">
+      <div className="h-full bg-[#2A7860] p-7">
+        <div className="flex flex-col items-center justify-center px-10 py-7 text-center text-white">
+          <div className="text-3xl font-semibold">{date}</div>
+          <div className="text-sm">{time}</div>
+        </div>
+      </div>
+      <div className="h-full] bg-[#f7eee3] p-7">
+        <div className="">
+          <h1 className="text-4xl font-semibold text-[#203e4e]">{title}</h1>
+          <p className="mt-2 text-[#203e4e]">{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Events;
