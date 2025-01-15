015aaab5e05e15c03eac10f1c1ecbddc40df095c
qhgill <qhgill2@gmail.com>
2025-01-14 19:42:21 -0800

    more change

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -3,7 +3,7 @@ import Image, { StaticImageData } from "next/image";
 
 const Demo = ({ name, image }: { name: string; image: StaticImageData }) => {
   return (
-    <div className="m-10 flex w-1/2 flex-col items-center border-4 border-leap-dark-green p-10">
+    <div className=" bg-black m-30 flex w-1/2 flex-col items-center border-4 border-leap-dark-green p-1">
       <Image className="aspect-square object-contain" src={image} alt="leap" />
       <p>{name} is cool</p>
     </div>
