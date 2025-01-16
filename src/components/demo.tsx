d3ab5eceb28efc92cb9ea223f7e5f844a83145cb
qhgill <qhgill2@gmail.com>
2025-01-16 12:53:01 -0800

    changes

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,13 +1,59 @@
 import React from "react";
-//import Image, { StaticImageData } from "next/image";
-//{ name, image }: { name: string; image: StaticImageData }
+
 const Demo = () => {
   return (
     <div className="border-2 border-black">
-      {/* <Image className="aspect-square object-contain" src={image} alt="leap" /> */}
       <p className="text-center font-leap">kevin is cool</p>
     </div>
   );
 };
 
 export default Demo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//{ name, image }: { name: string; image: StaticImageData }
