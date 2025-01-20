556e530252c44627f3a2fa892c06c3532293b79c
Medha Yasa <medha.yasa@gmail.com>
2025-01-20 15:53:45 -0800

    made about section text and title

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import About from "../../components/about/About";
 import Button from "../../components/about/Button";
+import AboutTextTitle from "../../components/about/AboutTextTitle";
 
 const AboutUs = () => {
   return (
     <div>
       <About />
       <Button text="View our constitution" link="#" />
+      <AboutTextTitle />
     </div>
   );
 };
---
+++ src/components/about/AboutTextTitle.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const AboutTextTitle = () => {
+  return (
+    <div className="font-jakarta text-center text-4xl font-bold text-black">
+      <p>
+        LEAP
+        <br />
+      </p>
+      <div className="font-jakarta text-center text-sm font-medium leading-6 tracking-widest text-black">
+        <p>
+          <br />
+          We are LEAP!, a.k.a. Loving our Emotions
+          <br />
+          as Asians & Pacific !slanders. We are a
+          <br />
+          club committed to mental health wellness
+          <br />
+          & awareness within API communities,
+          <br />
+          especially here at UCR. We feel it's
+          <br />
+          important we break cultural stigmas of
+          <br />
+          mental health to allow us to achieve a
+          <br />
+          strong & healthy well-being, both in mind
+          <br />
+          & body. With this, we hope to create a
+          <br />
+          safe & welcoming space to talk about our
+          <br />
+          emotions and experiences to grow
+          <br />
+          ourselves in every way.
+          <br />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutTextTitle;
