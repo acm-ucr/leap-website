aca6e9397e1f9354cdbc0285657a6d8f5df213a7
Andres Lopez <alope759@ucr.edu>
2025-01-22 13:36:35 -0800

    Created and formatted homepage title and text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Example from "../components/Example";
+import OurHome from "../components/homepage/homepage";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="Adjust" text2="these" text3="Props" />
+    <div>
+      <OurHome />
     </div>
   );
 };
