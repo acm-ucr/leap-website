fe2840bb562ddad44b3b577783b89f649393bb45
Computer Science <ccomputerscience23@gmail.com>
2025-01-22 01:10:41 -0800

    format

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
-      <div className="flex min-h-screen flex-wrap items-center justify-center #D5E2C9">
+      <div className="#D5E2C9 flex min-h-screen flex-wrap items-center justify-center">
         <FAQCard text="How do I become an active member of LEAP!?" />
       </div>
     </div>
--- src/components/about/FAQCard.tsx
+++ src/components/about/FAQCard.tsx
@@ -9,7 +9,7 @@ const FAQCard: React.FC<FAQCardProps> = ({ text }) => {
     <div className="relative m-4 flex flex-col items-center">
       <div className="top text-5xl text-leap-mid-green">♡</div>
       <div
-        className="square-lg aspect-[4/3] w-64 bg-leap-mid-green p-8 text-center text-lg font-leap text-white shadow-lg"
+        className="square-lg aspect-[4/3] w-64 bg-leap-mid-green p-8 text-center font-leap text-lg text-white shadow-lg"
         style={{
           boxShadow: "10px 10px 5px #324935",
         }}
