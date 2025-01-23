2776de912edd4c2ca5fd559a62872303cef37781
Andres Lopez <alope759@ucr.edu>
2025-01-23 15:41:02 -0800

    Code accomodated to only fulfill issue requests.

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -3,21 +3,11 @@ import React from "react";
 const Home = () => {
   return (
     <div className="h-screen bg-gradient-to-b from-white to-leap-mid-green">
-      <div className="py-20 text-center">
-        <h1 className="font-leap text-6xl font-bold text-leap-dark-green">
-          UCR LEAP
-        </h1>
-        <p className="mx-auto max-w-xl font-leap text-lg text-leap-dark-green">
-          LEAP!'s mission is to promote and encourage the acknowledgment and
-          health of mental health in the Asian & Pacific Islander community of
-          UCR.
-        </p>
-      </div>
       <div className="py-10">
-        <h2 className="mb-6 ml-6 font-leap text-3xl font-bold text-leap-dark-green underline">
+        <h2 className= "ml-6 mb-3 mt-80 font-leap text-3xl font-bold text-leap-dark-green underline">
           Our Core Values
         </h2>
-        <p className="mx-auto mb-10 ml-6 max-w-3xl font-leap text-leap-dark-green">
+        <p className="ml-6 max-w-3xl font-leap text-leap-dark-green">
           Loving our Emotions as Asian & Pacific Islanders, otherwise known as
           LEAP!, is a community of friends who advocate for safe spaces and
           mental health. We strive to make a safe space for everyone of every
