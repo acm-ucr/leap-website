c281e1786ccecf80feeeb96617e9ed7b21e2831e
Laiba Nasir <nasir.laiba.dev@gmail.com>
2025-01-26 01:57:32 -0800

    continue working on card component by fixing small things

--- src/components/about/ValueCard.tsx
+++ src/components/about/ValueCard.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 
+
+
 interface ValueCardProps {
   title: string;
   text: string;
 }
 
 const ValueCard: React.FC<ValueCardProps> = ({ title, text }) => {
+
+  
   // Assign class names to variables for readability and maintainability
-  const containerClasses = "flex flex-col items-center my-5 font-bold";
-  const titleClasses = "text-2xl text-[#3a593e] mb-4 text-center font-bold";
+  const containerClasses = "flex flex-col items-center my-8";
+  
+  const titleClasses = "text-3xl text-leap-dark-green mb-6 text-center font-bold tracking-widest";
+
   const boxClasses =
-    "bg-[#5e9679] p-5 shadow-[0_8px_10px_rgba(0.9,0.9,0.9,0.9)] max-w-[300px] text-center text-white text-base leading-relaxed relative";
+    "bg-leap-mid-green p-6 shadow-[0_4px_6px_rgba(0.9,1.9,0.9,1.9)] max-w-[300px] text-center text-white text-base leading-relaxed relative tracking-widest";
 
   return (
     <div className={containerClasses}>
