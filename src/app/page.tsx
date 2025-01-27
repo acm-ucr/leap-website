fe834776420d9e2d34ccef16c26ffc33f508d8d7
Shrey Kothari <shrey.gk1@gmail.com>
2025-01-27 12:03:32 -0800

    made fixes to image and structure of calling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import Description_Component from "@/components/home/Description_Component";
 
 const Home = () => {
   return (
-    <div> <Description_Component/> </div>
+    <div>
+      {" "}
+      <Description_Component />{" "}
+    </div>
   );
 };
 
