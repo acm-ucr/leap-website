9c9dd8cf9d0fd5a23ab2838fd964a06776e57e4d
Computer Science <ccomputerscience23@gmail.com>
2025-01-28 01:38:59 -0800

    added more FAQCard

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,8 +5,15 @@ const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
-      <div className="#D5E2C9 flex min-h-screen flex-wrap items-center justify-center">
-        <FAQCard text="How do I become an active member of LEAP!?" />
+      <div className="items-full flex min-h-screen flex-col justify-center">
+        <h1 className="mb-12 text-left text-4xl font-bold text-leap-dark-green">
+          Recruitment Questions
+        </h1>
+        <div className="flex flex-wrap items-center justify-center gap-6">
+          <FAQCard text="How do I become an active member of LEAP!?" />
+          <FAQCard text="What are some of the things/activities that LEAP! has done in the past?" />
+          <FAQCard text="Do any of the meetings require RSVP or payment to attend?" />
+        </div>
       </div>
     </div>
   );
