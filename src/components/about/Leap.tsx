ed283f096aaa496a8de0b4836b5fff2eca29cb0a
Shrey Kothari <shrey.gk1@gmail.com>
2025-01-28 18:56:33 -0800

    added text and title on the left and photo on the right; major adjustments needed

--- src/components/about/Leap.tsx
+++ src/components/about/Leap.tsx
@@ -5,14 +5,27 @@ const ButterflyImg = "/assets/about/bird.webp";
 
 const Leap = () => {
   return (
-    <div>
-      <Image
-        src={ButterflyImg}
-        alt="Butterfly"
-        width={300}
-        height={550}
-        className=""
-      />
+    <div className="flex items-center p-8">
+      <div className="w-2/3 pr-8 text-left">
+        <h2 className="text-2xl font-leap font-bold">LEAP</h2>
+        <p className="mt-4 text-gray-700">
+          We are LEAP!, a.k.a. Loving our Emotions as Asians & Pacific Islanders.
+          We are a club committed to mental health wellness & awareness within API communities,
+          especially here at UCR. We feel it is important we break cultural stigmas of mental health
+          to allow us to achieve a strong & healthy well-being, both in mind & body. With this,
+          we hope to create a safe & welcoming space to talk about our emotions and experiences to grow ourselves in every way.
+        </p>
+      </div>
+
+      <div className="w-1/3 flex justify-end">
+        <Image
+          src={ButterflyImg}
+          alt="Butterfly"
+          width={300}
+          height={550}
+          className=""
+        />
+      </div>
     </div>
   );
 };
