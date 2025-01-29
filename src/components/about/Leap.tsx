669d77572dc818439f13c972be7fae09308437cc
Shrey Kothari <shrey.gk1@gmail.com>
2025-01-28 18:17:47 -0800

    adding first edits to about

--- src/components/about/Leap.tsx
+++ src/components/about/Leap.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 const ButterflyImg = "/assets/about/bird.webp";
 
 const Leap = () => {
-    return(
-        <div>
-            <Image
-            src = {ButterflyImg}
-            alt = "Butterfly"
-            width={300}
-            height={550}
-            className=""
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Image
+        src={ButterflyImg}
+        alt="Butterfly"
+        width={300}
+        height={550}
+        className=""
+      />
+    </div>
+  );
 };
 
 export default Leap;
