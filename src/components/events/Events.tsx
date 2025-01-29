96aa10430ca3a337afaee5dcf871641811ba46c8
rishikpulipaka <rpuli007@ucr.edu>
2025-01-29 15:11:59 -0800

    Added tailwind colors instead of custom colors

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -9,8 +9,8 @@ interface EventProps {
 
 const Events: React.FC<EventProps> = ({ date, time, title, desc }) => {
   return (
-    <div className="grid h-[1/3vh] w-[1050px] grid-cols-7 items-center overflow-hidden rounded-lg">
-      <div className="col-span-1 flex h-full items-center justify-center bg-[#2A7860]">
+    <div className="grid h-[1/3vh] w-[1050px] grid-cols-7 items-center overflow-hidden rounded-lg font-leap">
+      <div className="col-span-1 flex h-full items-center justify-center bg-leap-mid-green">
         <div className="flex flex-col items-center justify-center px-10 py-7 text-center text-white">
           <div className="text-3xl font-semibold">{date}</div>
           <div className="text-sm">{time}</div>
@@ -18,8 +18,8 @@ const Events: React.FC<EventProps> = ({ date, time, title, desc }) => {
       </div>
       <div className="col-span-6 h-full bg-[#f7eee3]">
         <div className="p-7">
-          <h1 className="text-4xl font-semibold text-[#203e4e]">{title}</h1>
-          <p className="mt-2 text-[#203e4e]">{desc}</p>
+          <h1 className="text-4xl font-semibold text-leap-dark-green">{title}</h1>
+          <p className="mt-2 text-leap-dark-green">{desc}</p>
         </div>
       </div>
     </div>
