d1a9bdcef1c5a3cfd6d033e9d64e120a1a53c531
Shrey Kothari <shrey.gk1@gmail.com>
2025-01-29 18:35:17 -0800

    minor changes

--- src/components/about/Leap.tsx
+++ src/components/about/Leap.tsx
@@ -5,10 +5,10 @@ const ButterflyImg = "/assets/about/bird.webp";
 
 const Leap = () => {
   return (
-    <div className="flex items-center p-8">
-      <div className="w-2/3 pr-8 text-left">
-        <h2 className="font-leap text-5xl font-bold">LEAP</h2>
-        <p className="mt-4 text-black">
+    <div className="ml-24 flex items-center p-8 text-center">
+      <div className="w-2/3">
+        <h2 className="mb-14 font-leap text-7xl font-bold">LEAP</h2>
+        <p className="max-w-1xl mt-14 font-leap text-3xl font-semibold leading-loose text-black">
           We are LEAP!, a.k.a. Loving our Emotions as Asians & Pacific
           Islanders. We are a club committed to mental health wellness &
           awareness within API communities, especially here at UCR. We feel it
@@ -19,12 +19,12 @@ const Leap = () => {
         </p>
       </div>
 
-      <div className="mr-36 flex w-1/3 justify-end">
+      <div className="mr-28 flex w-1/3 justify-end">
         <Image
           src={ButterflyImg}
           alt="Butterfly"
-          width={300}
-          height={550}
+          width={400}
+          height={1000}
           className=""
         />
       </div>
