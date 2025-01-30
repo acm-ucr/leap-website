3c5e373b8e1d291eab7d29607cf58e7486fea516
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2025-01-29 21:06:30 -0800

    Update Leap.tsx

--- src/components/about/Leap.tsx
+++ src/components/about/Leap.tsx
@@ -5,10 +5,9 @@ const ButterflyImg = "/assets/about/bird.webp";
 
 const Leap = () => {
   return (
-    <div className="ml-24 flex items-center p-8 text-center">
-      <div className="w-2/3">
+    <div className="w-2/3 ml-24 flex items-center p-8 text-center">
         <h2 className="mb-14 font-leap text-7xl font-bold">LEAP</h2>
-        <p className="max-w-1xl mt-14 font-leap text-3xl font-semibold leading-loose text-black">
+        <div className="max-w-1xl mt-14 font-leap text-3xl font-semibold leading-loose text-black">
           We are LEAP!, a.k.a. Loving our Emotions as Asians & Pacific
           Islanders. We are a club committed to mental health wellness &
           awareness within API communities, especially here at UCR. We feel it
@@ -16,8 +15,7 @@ const Leap = () => {
           achieve a strong & healthy well-being, both in mind & body. With this,
           we hope to create a safe & welcoming space to talk about our emotions
           and experiences to grow ourselves in every way.
-        </p>
-      </div>
+        </div>
 
       <div className="mr-28 flex w-1/3 justify-end">
         <Image
