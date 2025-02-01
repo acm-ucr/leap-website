8922fc5914984ef39cd9bf44daaa28fff1e2ea09
nidheesh-m-vaharia <nvakh003@ucr.edu>
2025-01-31 17:00:50 -0800

    Carousel Implimented

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Carousel } from "../Carousel";
 
 const Gallery = () => {
-  return <div>Gallery Page</div>;
+  return (
+    <div>
+      Gallery Page
+      <Carousel />
+    </div>
+  );
 };
 
 export default Gallery;
