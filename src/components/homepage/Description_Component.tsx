3ea3147ee79bd71bd71013a0d8be2755b7f6c05c
Andres Lopez <alope759@ucr.edu>
2025-01-31 19:07:25 -0800

    Text has been formatted to follow the figma design.

--- src/components/homepage/Description_Component.tsx
+++ src/components/homepage/Description_Component.tsx
@@ -4,15 +4,15 @@ const Description_Component = () => {
   return (
     <div className="h-screen bg-gradient-to-b from-leap-light-green to-leap-mid-green">
       <div className="py-10">
-        <h2 className="mb-3 ml-6 mt-80 font-leap text-3xl font-bold text-white">
+        <h2 className="ml-[96px] mt-80 font-leap text-[60px] font-bold text-white">
           Our Core Values
         </h2>
-        <p className="ml-6 max-w-3xl font-leap text-white">
-          Loving our Emotions as Asian & Pacific Islanders, otherwise known as
+        <p className="w-3xl ml-[96px] mr-[500px] mt-10 font-leap text-[22px] text-white">
+          Loving our Emotions as Asian & Pacific !slanders, otherwise known as
           LEAP!, is a community of friends who advocate for safe spaces and
           mental health. We strive to make a safe space for everyone of every
           background to chat, destress, make friends, and learn about mental
-          health in the AAPI community. Come join us to hang out!
+          health in the AAPI community. Come join us to hangout!
         </p>
       </div>
     </div>
