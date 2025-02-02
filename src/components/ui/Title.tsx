1b3f6839563270cde77e4b2ed1350869175fc6e3
jewynny <jgmh003@gmail.com>
2025-02-02 11:42:54 -0800

    removed accidental second component, color can now be passed in

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -3,13 +3,10 @@ const Title: React.FC<{ title: string; color: string }> = ({
   color = "leap-dark-green",
 }) => {
   return (
-    <div className="m-8">
+    <div className="m-4">
       <p
-        className="text-alignment mx-auto text-center font-leap text-8xl font-semibold text-leap-dark-green"
-        style={{ color }}
+        className={`text-alignment mx-auto text-center font-leap text-8xl font-semibold text-${color}`}
       >
-        {" "}
-        {/* color doesnt work here for now */}
         {title}
       </p>
     </div>
