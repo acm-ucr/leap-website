764794e974f9b91f26435d9173fb55d410195ecd
Andres Lopez <alope759@ucr.edu>
2025-02-02 16:38:45 -0800

    Implemented flexbox formatting & centered text.

--- src/components/homepage/Description_Component.tsx
+++ src/components/homepage/Description_Component.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Description_Component = () => {
   return (
-    <div className="h-screen bg-gradient-to-b from-leap-light-green to-leap-mid-green">
-      <div className="py-10">
-        <h2 className="ml-[96px] mt-80 font-leap text-[60px] font-bold text-white">
+    <div className="flex h-screen items-center justify-center bg-gradient-to-b from-leap-light-green to-leap-mid-green">
+      <div className="max-w-3xl px-10 text-center md:flex md:flex-col md:items-start">
+        <h2 className="font-leap text-[60px] font-bold text-white">
           Our Core Values
         </h2>
-        <p className="w-3xl ml-[96px] mr-[500px] mt-10 font-leap text-[22px] text-white">
+        <p className="mt-10 font-leap text-[22px] text-white">
           Loving our Emotions as Asian & Pacific !slanders, otherwise known as
           LEAP!, is a community of friends who advocate for safe spaces and
           mental health. We strive to make a safe space for everyone of every
