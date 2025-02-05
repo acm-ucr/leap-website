1b469b72740c1623b08f06196ce58572545d6d9a
Computer Science <ccomputerscience23@gmail.com>
2025-02-04 23:33:34 -0800

    background added

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { faqData } from "./faqData";
-import FAQCard from "./FAQCard";
+import FAQSection from "./question";
 
 const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
-      <div className="items-full flex min-h-screen flex-col justify-center">
-        <h1 className="ml-12 text-left text-4xl font-bold text-leap-dark-green">
-          Recruitment Questions
-        </h1>
-        <div className="flex flex-wrap items-center justify-center gap-6">
-          {faqData.map((FAQItem, index) => (
-            <FAQCard key={index} text={FAQItem.text} />
-          ))}
-        </div>
-      </div>
+      <FAQSection />
     </div>
   );
 };
