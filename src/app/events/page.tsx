5982711b95b4efd04af812b3bac6101beebe4cbc
jewynny <jgmh003@gmail.com>
2025-02-06 11:34:26 -0800

    Created upcomming title component with underline and used it in events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,6 +1,7 @@
 import Events from "../../components/events/Events";
 import Header from "../../components/board/header";
 import { events } from "@/data/events";
+import UpcommingTitle from "@/components/events/UpcommingTitle";
 
 const OurEvents = () => {
   return (
@@ -9,6 +10,7 @@ const OurEvents = () => {
         title={"Events"}
         subtitle={"Explore when our next event is and make sure to stop by."}
       />
+      <UpcommingTitle title="Upcomming Events" />
       {events.map((event) => (
         <Events
           title={event.title}
