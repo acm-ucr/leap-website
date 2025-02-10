395dfa32c22bf042b6d7d5da434a454faf609d46
Computer Science <ccomputerscience23@gmail.com>
2025-02-09 16:25:47 -0800

    responsive FQA card

--- src/components/about/FAQCard.tsx
+++ src/components/about/FAQCard.tsx
@@ -4,12 +4,16 @@ import React from "react";
 interface FAQCardProps {
   text: string;
 }
+
 const FAQCard: React.FC<FAQCardProps> = ({ text }) => {
   return (
     <div className="relative m-4 flex flex-col items-center">
-      <div className="top text-5xl text-leap-mid-green">♡</div>
+      {/*Icon*/}
+      <div className="top text-3xl text-leap-mid-green md:text-5xl">♡</div>
+
+      {/*Card*/}
       <div
-        className="square-lg aspect-[4/3] w-64 bg-leap-mid-green p-8 text-center font-leap text-lg text-white shadow-lg"
+        className="square-lg aspect-[4/3] w-11/12 bg-leap-mid-green p-6 text-center font-leap text-xl text-white shadow-xl sm:w-64 md:w-72 md:p-8 lg:w-80"
         style={{
           boxShadow: "10px 10px 5px #324935",
         }}
