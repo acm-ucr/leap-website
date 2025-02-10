1ba935d7a9309270b41b46713a6cae3c2c185148
lbojo006 <luisbojorquezgoal@gmail.com>
2025-02-09 21:31:43 -0800

    Mapped Gallery

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PicturesGrid from "./PicturesGrid";
 import BackgroundImage from "@/public/assets/bg-error.webp";
 import Image from "next/image";
+import { Carousel } from "../Carousel";
 
 const Gallery = () => {
   return (
@@ -9,12 +10,13 @@ const Gallery = () => {
       <Image
         src={BackgroundImage}
         alt="Gallery Background"
-        className="absolute -z-10 min-h-screen w-screen"
+        className="absolute -z-10 min-h-screen w-screen h-screen"
       />
       <div className="mt-12 pt-12">
         <h1 className="mt-5 pt-12 text-center font-leap text-4xl font-bold">
           Gallery Page
         </h1>
+        <Carousel />
         <PicturesGrid />
       </div>
     </div>
