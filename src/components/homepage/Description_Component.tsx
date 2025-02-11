2ccc4f97da2d2712fd17c3ec08a7375b06f1cee7
Andres Lopez <alope759@ucr.edu>
2025-02-11 13:42:34 -0800

    Updated file to align with tailwind lingo + added mobile responsiveness.

--- src/components/homepage/Description_Component.tsx
+++ src/components/homepage/Description_Component.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Description_Component = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-gradient-to-b from-leap-light-green to-leap-mid-green">
-      <div className="max-w-3xl px-10 text-center md:flex md:flex-col md:items-start">
-        <h2 className="font-leap text-[60px] font-bold text-white">
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-full text-left sm:w-3/4 lg:w-1/2">
+        <h2 className="font-leap text-4xl font-bold text-white sm:text-5xl lg:text-7xl">
           Our Core Values
         </h2>
-        <p className="mt-10 font-leap text-[22px] text-white">
+        <p className="mt-10 font-leap text-lg text-white sm:text-lg">
           Loving our Emotions as Asian & Pacific !slanders, otherwise known as
           LEAP!, is a community of friends who advocate for safe spaces and
           mental health. We strive to make a safe space for everyone of every
