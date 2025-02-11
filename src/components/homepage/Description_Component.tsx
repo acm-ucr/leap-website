38119958f5017eab35ae71456cda6780b2012a9a
qhgill <qhgill2@gmail.com>
2025-02-11 14:04:48 -0800

    fixes

--- src/components/homepage/Description_Component.tsx
+++ src/components/homepage/Description_Component.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Description_Component = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="w-full text-left sm:w-3/4 lg:w-1/2">
+    <div className="flex items-center justify-center">
+      <div className="w-3/4 text-left">
         <h2 className="font-leap text-4xl font-bold text-white sm:text-5xl lg:text-7xl">
           Our Core Values
         </h2>
-        <p className="mt-10 font-leap text-lg text-white sm:text-lg">
+        <p className="mt-10 font-leap text-xl text-white md:text-2xl">
           Loving our Emotions as Asian & Pacific !slanders, otherwise known as
           LEAP!, is a community of friends who advocate for safe spaces and
           mental health. We strive to make a safe space for everyone of every
