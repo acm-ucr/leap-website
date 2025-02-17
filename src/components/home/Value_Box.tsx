86dc659d4769c02cb4e0ba28d10f55748b389459
Shrey Kothari <shrey.gk1@gmail.com>
2025-02-16 19:23:50 -0800

    small size done; photo needs to be moved up

--- src/components/home/Value_Box.tsx
+++ src/components/home/Value_Box.tsx
@@ -8,7 +8,7 @@ const Value_Box = () => {
         {topics.map((topic, index) => (
           <div
             key={index}
-            className="flex h-40 w-72 items-center justify-center border-2 border-white bg-leap-mid-green p-2 text-center font-leap text-white shadow-lg shadow-leap-light-green lg:text-3xl"
+            className="md:h-30 md:text-2.5xl flex h-20 w-40 items-center justify-center border-2 border-white bg-leap-mid-green p-2 text-center font-leap text-2xl text-white shadow-lg shadow-leap-light-green md:w-56 lg:h-40 lg:w-72 lg:text-3xl"
           >
             {topic}
           </div>
