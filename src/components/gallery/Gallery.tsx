8bc240bee0aa31e527f253b6dc694a6844222f0c
lbojo006 <luisbojorquezgoal@gmail.com>
2025-02-18 19:21:33 -0800

    Animated Gallery

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,8 +1,10 @@
+"use client";
 import React from "react";
 import PicturesGrid from "./PicturesGrid";
 import BackgroundImage from "@/public/assets/bg-error.webp";
 import Image from "next/image";
 import { Carousel } from "../Carousel";
+import { motion } from "motion/react";
 
 const Gallery = () => {
   return (
@@ -15,13 +17,32 @@ const Gallery = () => {
         className="absolute -z-10"
       />
       <div className="relative mt-12 w-5/6 pt-12">
-        <h1 className="mt-5 pt-12 text-center font-leap text-4xl font-bold">
+        <motion.h1
+          initial={{ x: -50, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="mt-5 pt-12 text-center font-leap text-4xl font-bold"
+        >
           Gallery Page
-        </h1>
-        <Carousel />
+        </motion.h1>
+
+        <motion.div
+          initial={{ x: -50, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <Carousel />
+        </motion.div>
+
         <div className="mt-5 flex justify-center pt-12">
           <h2 className="inline-block border-b-2 border-black text-center font-leap text-2xl font-bold">
-            2024-2025
+            <motion.div
+              initial={{ x: -50, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+            >
+              2024-2025
+            </motion.div>
           </h2>
         </div>
         <PicturesGrid />
