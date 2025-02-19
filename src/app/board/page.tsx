c14197e02957152a5d68c9675994156298019b40
Josh <joshuacitrin@gmail.com>
2025-02-18 19:36:22 -0800

    Incomplete memberboard

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,5 +1,6 @@
 import Board from "../../components/board/Board";
 import Header from "../../components/board/header";
+import BoardMemberCards from "../../components/board/BoardMemberCards";
 
 const OurBoard = () => {
   return (
@@ -10,6 +11,20 @@ const OurBoard = () => {
         subtitle={
           "Individuals dedicated to helping students overcome mental health issues."
         }
+      />
+      <BoardMemberCards
+        name1={"Cathy Nguyen"}
+        role1={"President"}
+        name2={"Ethan Ly"}
+        role2={"Vice President"}
+        name3={"Abby Hinojales"}
+        role3={"Secretary"}
+        name4={"Debdeep Bandyopadhyay"}
+        role4={"Treasurer"}
+        name5={"Denise Hicks"}
+        role5={"Publicist"}
+        name6={"Sneha Panda"}
+        role6={"Events Coordinator"}
       />
     </div>
   );
