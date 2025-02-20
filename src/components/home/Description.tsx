072cec7a9bce1b84d910060edc184f559bd5caf7
Shrey Kothari <shrey.gk1@gmail.com>
2025-02-19 18:20:53 -0800

    requested edits

--- src/components/home/Description.tsx
+++ src/components/home/Description.tsx
@@ -4,11 +4,11 @@ import FlowerImg from "@/public/assets/home/description.webp";
 
 const Description = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="ml-5 flex items-center justify-center">
       <Image
         src={FlowerImg}
         alt="Flowers"
-        className="h-[300px] w-[200px] md:h-[450px] md:w-[350px] lg:h-[650px] lg:w-[475px]"
+        className="h-[300px] w-[200px] md:h-[450px] md:w-[300px] lg:h-[650px] lg:w-[475px]"
       />
     </div>
   );
--- src/components/home/Value_Box.tsx
+++ src/components/home/Value_Box.tsx
@@ -4,11 +4,11 @@ const Value_Box = () => {
   const topics = ["Awareness", "Community", "Safety", "Mental Health"];
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="grid w-full max-w-xs grid-cols-1 gap-4 lg:max-w-md">
+      <div className="w-xs lg:w-md grid w-full grid-cols-1 gap-4">
         {topics.map((topic, index) => (
           <div
             key={index}
-            className="flex h-20 w-40 items-center justify-center border-2 border-white bg-leap-mid-green p-2 text-center font-leap text-2xl text-white shadow-lg shadow-leap-light-green md:w-56 lg:h-40 lg:w-72 lg:text-3xl"
+            className="w-15/16 md:w-14/15 lg:w-21/24 flex h-20 items-center justify-center border-2 border-white bg-leap-mid-green p-2 text-center font-leap text-2xl text-white shadow-lg shadow-leap-light-green lg:h-40 lg:text-3xl"
           >
             {topic}
           </div>
