77f37affc0aefa1a9be91825e4501e52f24649f5
Shrey Kothari <shrey.gk1@gmail.com>
2025-02-20 13:30:08 -0800

    initial animate edits

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
 import BackgroundImage from "@/public/assets/bg-error.webp";
 import Image from "next/image";
+import React from "react";
+import { motion } from "motion/react";
+
 const NotFound = () => {
   return (
     <div className="relative flex h-[90vh] flex-col items-center justify-center overflow-hidden">
@@ -9,15 +13,31 @@ const NotFound = () => {
         alt="404"
         className="absolute -z-20 min-h-screen w-screen"
       />
-      <h1 className="mb-2 text-5xl font-bold text-leap-text md:text-7xl">
+      <motion.div
+        initial={{ x: -50 }}
+        whileInView={{ x: 0 }}
+        transition={{ duration: 1 }}
+        className="mb-2 text-5xl font-bold text-leap-text md:text-7xl"
+      >
         404
-      </h1>
-      <h3 className="mb-6 text-lg font-semibold text-leap-text md:text-2xl">
+      </motion.div>
+      <motion.div
+        initial={{ x: -50 }}
+        whileInView={{ x: 0 }}
+        transition={{ duration: 1 }}
+        className="mb-6 text-lg font-semibold text-leap-text md:text-2xl"
+      >
         Page Not Found
-      </h3>
-      <Link href="/" className="bg-leap-mid-green p-4 text-leap-text">
-        Back to Home
-      </Link>
+      </motion.div>
+      <motion.div
+        initial={{ x: -50 }}
+        whileInView={{ x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Link href="/" className="bg-leap-mid-green p-4 text-leap-text">
+          Back to Home
+        </Link>
+      </motion.div>
     </div>
   );
 };
