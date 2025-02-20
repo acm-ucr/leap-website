eb842192c03c3a6576220141ee62c2fa06beecd0
Derrick-Mao <derrickmao03@gmail.com>
2025-02-20 14:29:16 -0800

    n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body className={krub.className}>
-        {/* <Navbar /> */}
+        <Navbar />
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
         <Footer />
       </body>
