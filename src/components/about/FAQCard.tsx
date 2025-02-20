92406752360dde5a50de1fcf22b95e3e2aebe83d
lbojo006 <luisbojorquezgoal@gmail.com>
2025-02-20 08:48:24 -0800

    Fixed text being backwards

--- src/components/about/FAQCard.tsx
+++ src/components/about/FAQCard.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "motion/react";
-import { useState } from "react";
+
 interface FAQCardProps {
   text: string;
   backText: string;
@@ -13,17 +13,32 @@ const FAQCard: React.FC<FAQCardProps> = ({ text, backText }) => {
   return (
     <motion.div
       whileHover={{ scale: 1.1 }}
-      transition={{ duration: 1 / 2 }}
+      transition={{ duration: 0.5 }}
       animate={{ rotateY: flipped ? 180 : 0 }}
       onClick={() => setFlipped((prevState) => !prevState)}
       className="relative m-4 flex flex-col items-center"
+      style={{ transformStyle: "preserve-3d" }}
     >
       {/* Icon */}
       <div className="top text-4xl text-leap-mid-green md:text-5xl">♡</div>
 
       {/* Card */}
-      <div className="square-lg flex aspect-[4/3] w-8/12 items-center justify-center bg-leap-mid-green p-6 text-center font-leap text-xl text-white shadow-lg shadow-leap-dark-green sm:w-64 md:w-72 md:text-lg lg:w-80 lg:text-2xl">
-        <p>{flipped ? backText : text}</p>
+      <div
+        className="square-lg flex aspect-[4/3] w-8/12 items-center justify-center bg-leap-mid-green p-6 text-center font-leap text-xl text-white shadow-lg shadow-leap-dark-green sm:w-64 md:w-72 md:text-lg lg:w-80 lg:text-2xl"
+        style={{ position: "relative", transformStyle: "preserve-3d" }}
+      >
+        <div
+          className="absolute flex h-full w-full items-center justify-center"
+          style={{ backfaceVisibility: "hidden" }}
+        >
+          <p>{text}</p>
+        </div>
+        <div
+          className="absolute flex h-full w-full items-center justify-center"
+          style={{ backfaceVisibility: "hidden", transform: "rotateY(180deg)" }}
+        >
+          <p>{backText}</p>
+        </div>
       </div>
     </motion.div>
   );
