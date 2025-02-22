cddf3414b1c7580b71a10e402e9467a637220e6c
Laiba Nasir <nasir.laiba.dev@gmail.com>
2025-02-22 14:42:16 -0800

    experiemting with header width bug

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -9,7 +9,7 @@ interface EventProps {
 
 const Events: React.FC<EventProps> = ({ date, time, title, desc }) => {
   return (
-    <div className="grid grid-cols-7 items-center overflow-hidden rounded-lg font-leap sm:h-1/3 sm:w-full">
+    <div className="mx-auto grid w-full max-w-6xl grid-cols-7 items-center overflow-hidden rounded-lg font-leap sm:h-1/3">
       <div className="col-span-1 flex h-full items-center justify-center bg-leap-mid-green">
         <div className="flex flex-col items-center justify-center px-10 py-7 text-center text-white">
           <div className="text-xl font-semibold md:text-3xl lg:text-4xl">
