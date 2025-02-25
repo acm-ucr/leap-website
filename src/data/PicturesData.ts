639596b7164de0e564e3068e6344b5f2a2c0da90
qhgill <qhgill2@gmail.com>
2025-02-24 17:10:29 -0800

    destructuring

--- src/data/PicturesData.ts
+++ src/data/PicturesData.ts
@@ -1,87 +1,87 @@
 interface PicturesData {
   className: string;
-  Pictures: string;
-  Alt: string;
-  ImgProperties?: string;
+  pictures: string;
+  alt: string;
+  imgProperties?: string;
 }
 
 export const PicturesData: PicturesData[] = [
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid1.webp",
-    Alt: "Image 1",
+    pictures: "/gallery/grid/grid1.webp",
+    alt: "Image 1",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid2.webp",
-    Alt: "Image 2",
+    pictures: "/gallery/grid/grid2.webp",
+    alt: "Image 2",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid3.webp",
-    Alt: "Image 3",
+    pictures: "/gallery/grid/grid3.webp",
+    alt: "Image 3",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid5.webp",
-    Alt: "Image 5",
+    pictures: "/gallery/grid/grid5.webp",
+    alt: "Image 5",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid6.webp",
-    Alt: "Image 6",
+    pictures: "/gallery/grid/grid6.webp",
+    alt: "Image 6",
   },
   {
     className:
       "flex justify-center items-center row-span-2 col-span-2  md:row-span-4 lg:row-span-4",
-    Pictures: "/gallery/grid/grid4.webp",
-    Alt: "Image 4",
+    pictures: "/gallery/grid/grid4.webp",
+    alt: "Image 4",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid7.webp",
-    Alt: "Image 7",
+    pictures: "/gallery/grid/grid7.webp",
+    alt: "Image 7",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid8.webp",
-    Alt: "Image 8",
+    pictures: "/gallery/grid/grid8.webp",
+    alt: "Image 8",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid9.webp",
-    Alt: "Image 9",
+    pictures: "/gallery/grid/grid9.webp",
+    alt: "Image 9",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid10.webp",
-    Alt: "Image 10",
+    pictures: "/gallery/grid/grid10.webp",
+    alt: "Image 10",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid11.webp",
-    Alt: "Image 11",
+    pictures: "/gallery/grid/grid11.webp",
+    alt: "Image 11",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid12.webp",
-    Alt: "Image 12",
+    pictures: "/gallery/grid/grid12.webp",
+    alt: "Image 12",
   },
   {
     className: "flex justify-center items-center md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid13.webp",
-    Alt: "Image 13",
+    pictures: "/gallery/grid/grid13.webp",
+    alt: "Image 13",
   },
   {
     className:
       "flex justify-center items-center col-span-2 md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid15.webp",
-    Alt: "Image 15",
+    pictures: "/gallery/grid/grid15.webp",
+    alt: "Image 15",
   },
   {
     className:
       "flex justify-center items-center col-span-2 md:row-span-2 lg:row-span-2",
-    Pictures: "/gallery/grid/grid14.webp",
-    Alt: "Image 14",
+    pictures: "/gallery/grid/grid14.webp",
+    alt: "Image 14",
   },
 ];
