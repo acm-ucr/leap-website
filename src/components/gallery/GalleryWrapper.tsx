6f289267f11b61c815adf4defc21113b518233c3
lbojo006 <luisbojorquezgoal@gmail.com>
2025-02-24 21:00:13 -0800

    Made Galery Padding Adjustments

--- src/components/gallery/GalleryWrapper.tsx
+++ src/components/gallery/GalleryWrapper.tsx
@@ -15,17 +15,17 @@ const GalleryWrapper = () => {
         objectFit="cover"
         className="absolute -z-10"
       />
-      <div className="relative mt-12 w-5/6 sm:pt-8 md:pt-10 lg:pt-12">
+      <div className="relative mt-3 w-5/6 md:mt-10 lg:mt-12">
         <motion.div
           initial={{ x: -50, opacity: 0 }}
           whileInView={{ x: 0, opacity: 1 }}
           transition={{ duration: 1 }}
-          className="text-center font-leap md:mt-8 md:py-8 lg:mt-10 lg:py-10"
+          className="text-center font-leap py-3 md:py-8 lg:py-10"
         >
           <div className="text-leap place-self-center pb-10 pt-40 text-3xl font-bold text-leap-dark-green sm:text-4xl md:text-4xl lg:text-5xl">
             <p>Gallery</p>
           </div>
-          <div className="text-leap md:pb-15 place-self-center px-20 pb-8 pt-4 text-base font-semibold text-leap-dark-green sm:pb-10 sm:text-lg md:text-xl lg:pb-20 lg:text-2xl">
+          <div className="text-leap md:pb-15 place-self-center px-20 pb-4 pt-4 text-base font-semibold text-leap-dark-green sm:pb-10 sm:text-lg md:text-xl lg:pb-20 lg:text-2xl">
             <p>Explore memories and photos from our past events</p>
           </div>
         </motion.div>
