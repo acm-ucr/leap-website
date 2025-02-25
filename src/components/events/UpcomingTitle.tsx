d7d334a0b0b2c93922d301a2f9f4c5f4c6682b90
qhgill <qhgill2@gmail.com>
2025-02-24 16:28:24 -0800

    fixed p tag

--- src/components/events/UpcomingTitle.tsx
+++ src/components/events/UpcomingTitle.tsx
@@ -4,7 +4,7 @@ const UpcomingTitle: React.FC<{ title: string }> = ({ title }) => {
       <p className="text-alignment mx-auto text-center font-leap text-5xl font-semibold text-leap-dark-green">
         {title}
       </p>
-      <p className="relative m-2 mx-auto w-1/6 border-b-4 border-leap-mid-green p-2"></p>
+      <div className="relative m-2 mx-auto w-1/6 border-b-4 border-leap-mid-green p-2" />
     </div>
   );
 };
