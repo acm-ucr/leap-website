fe271032364235891f4e5c1170dcf5a375d16389
Medha Yasa <medha.yasa@gmail.com>
2025-02-25 20:45:09 -0800

    added background for about header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Image, { StaticImageData } from "next/image";
 
 interface HeaderProps {
   title: string;
-  subtitle: string;
+  subtitle: React.ReactNode;
   background: StaticImageData;
 }
 
@@ -14,11 +14,11 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle, background }) => {
         className="absolute -z-20 h-[60vh] w-full md:flex"
         alt="header background"
       />
-      <div className="text-leap place-self-center pt-40 text-5xl text-leap-dark-green">
+      <div className="text-leap place-self-center pt-64 text-5xl text-leap-dark-green">
         {title}
       </div>
       ;
-      <div className="text-leap place-self-center px-20 pb-32 pt-4 text-2xl text-leap-dark-green">
+      <div className="text-leap place-self-center px-20 pb-20 pt-4 text-2xl text-leap-dark-green">
         <p> {subtitle}</p>
       </div>
     </div>
