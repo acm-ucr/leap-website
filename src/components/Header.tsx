76e47a7e8aa752058f1de261332ad824ccf2249e
qhgill <qhgill2@gmail.com>
2025-02-25 19:33:29 -0800

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle, background }) => {
         className="absolute -z-20"
         alt="header background"
       />
-      <div className="text-leap place-self-center pb-10 pt-52 md:pt-60 text-3xl font-bold text-leap-dark-green sm:text-4xl md:text-4xl lg:text-5xl">
+      <div className="text-leap place-self-center pb-10 pt-52 text-3xl font-bold text-leap-dark-green sm:text-4xl md:pt-60 md:text-4xl lg:text-5xl">
         {title}
       </div>
       <div className="text-leap md:pb-15 place-self-center px-20 pb-8 pt-4 text-base font-semibold text-leap-dark-green sm:pb-10 sm:text-lg md:text-xl lg:pb-20 lg:text-2xl">
