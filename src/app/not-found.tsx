b09b9c8151d0170d842fc162601e42ba28687aa3
qhgill <qhgill2@gmail.com>
2025-02-27 13:44:27 -0800

    remove react import

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,6 @@
 "use client";
 import BackgroundImage from "@/public/assets/backgrounds/BackgroundError.webp";
 import Image from "next/image";
-import React from "react";
 import { motion } from "framer-motion";
 import Button from "../components/Button";
 
