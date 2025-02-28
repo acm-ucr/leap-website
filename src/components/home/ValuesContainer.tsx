30289763152e00bd3f1d3c33c461d18a98a4355c
Andres Lopez <alope759@ucr.edu>
2025-02-28 10:13:28 -0800

    Individually animated value cards on homepage.

--- src/components/home/ValuesContainer.tsx
+++ src/components/home/ValuesContainer.tsx
@@ -8,21 +8,15 @@ const ValuesContainer = () => {
     <div className="flex min-h-screen flex-row items-center justify-center lg:pb-24 lg:pt-8">
       <motion.div
         className="flex items-center justify-center md:mt-20 lg:mt-16"
-        initial={{ opacity: 0 }}
-        whileInView={{ x: ["-5%", "0%"], y: ["20%", "0%"], opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.25 }}
       >
         <Description />
       </motion.div>
 
       <div className="mx-6 flex items-center justify-center md:mx-24 md:mt-20 lg:mx-40 lg:mt-16">
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ y: ["20%", "0%"], x: ["10%", "0%"], opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.1 }}
-        >
-          <ValueBox />
-        </motion.div>
+        <ValueBox />
       </div>
     </div>
   );
