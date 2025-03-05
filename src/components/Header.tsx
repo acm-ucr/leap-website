38e7363b4e7120f07c510fd5b8f891e136387032
Andres Lopez <alope759@ucr.edu>
2025-03-05 14:07:41 -0800

    Package-lock updated and Header has a small tweak.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle, background }) => {
       <Image
         src={background}
         layout="fill"
-        className="absolute -z-20"
+        className="absolute-z-20"
         alt="header background"
       />
       <motion.div
