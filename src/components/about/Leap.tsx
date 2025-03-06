3ecccd88492be8112429c0d00fed5cb05cc293f4
Andres Lopez <alope759@ucr.edu>
2025-03-06 14:07:14 -0800

    About Page Formatting

--- src/components/about/Leap.tsx
+++ src/components/about/Leap.tsx
@@ -48,10 +48,10 @@ const Leap = () => {
         </motion.p>
       </div>
 
-      <div className="lg:ml-26 hidden flex-col items-center p-8 text-center sm:ml-14 sm:flex md:ml-20 md:flex-row">
-        <div className="w-2/3 text-center font-leap">
+      <div className="lg:ml-26 hidden flex-col justify-between p-8 text-center sm:ml-14 sm:flex md:ml-20 md:flex-row">
+        <div className="w-1/2 p-4 text-center font-leap">
           <motion.div
-            className="text-3xl font-bold md:text-4xl lg:text-5xl"
+            className="mt-14 text-3xl font-bold md:text-4xl lg:text-5xl"
             initial={{ opacity: 0, y: -50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
@@ -75,7 +75,7 @@ const Leap = () => {
         </div>
 
         <motion.div
-          className="flex w-full justify-center"
+          className="flex w-1/2 justify-center"
           initial={{ opacity: 0, scale: 0.8 }}
           whileInView={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8, delay: 0.2 }}
