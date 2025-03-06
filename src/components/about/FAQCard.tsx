b49b5ffd2cb701a3c89bf79d2901a0c7a9a7e04e
Shrey Kothari <shrey.gk1@gmail.com>
2025-03-06 12:57:13 -0800

    final edits

--- src/components/about/FAQCard.tsx
+++ src/components/about/FAQCard.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import { motion } from "motion/react";
 import { Heart } from "lucide-react";
 
@@ -9,14 +8,10 @@ interface FAQCardProps {
 }
 
 const FAQCard: React.FC<FAQCardProps> = ({ text, backText }) => {
-  const [flipped, setFlipped] = useState(false);
-
   return (
     <motion.div
-      whileHover={{ scale: 1.1 }}
+      whileHover={{ scale: 1.1, rotateY: 180 }}
       transition={{ duration: 0.5 }}
-      animate={{ rotateY: flipped ? 180 : 0 }}
-      onClick={() => setFlipped((prevState) => !prevState)}
       className="relative m-4 flex cursor-pointer flex-col items-center"
       style={{ transformStyle: "preserve-3d" }}
     >
@@ -24,16 +19,16 @@ const FAQCard: React.FC<FAQCardProps> = ({ text, backText }) => {
         className="top pb-5 text-4xl text-leap-mid-green md:text-5xl"
         style={{ position: "relative", transformStyle: "preserve-3d" }}
       >
-        <Heart style={{ backfaceVisibility: "hidden" }}></Heart>
+        <Heart style={{ backfaceVisibility: "hidden" }} />
         <Heart
           fill="#4F8C6C"
           className="absolute inset-0 flex items-center justify-center"
           style={{ backfaceVisibility: "hidden", transform: "rotateY(180deg)" }}
-        ></Heart>
+        />
       </div>
 
       <div
-        className="square-lg flex aspect-[4/3] w-64 items-center justify-center bg-leap-mid-green p-6 text-center font-leap text-xl text-white shadow-lg shadow-leap-dark-green md:w-72 md:text-lg lg:w-80 lg:text-2xl"
+        className="square-lg flex aspect-[4/3] w-64 items-center justify-center bg-leap-mid-green p-6 text-center font-leap text-xl text-white shadow-lg shadow-black md:w-72 md:text-lg lg:w-80 lg:text-2xl"
         style={{ position: "relative", transformStyle: "preserve-3d" }}
       >
         <div
