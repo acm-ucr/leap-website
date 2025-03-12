6e16077c8f2d2f377f265f2026a9e844a29e1ccd
Josh <joshuacitrin@gmail.com>
2025-03-11 19:06:13 -0700

    Finished animation with on scroll effect

--- src/components/board/BoardWrapper.tsx
+++ src/components/board/BoardWrapper.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import BackgroundImage from "@/public/assets/backgrounds/BackgroundBoard.webp";
 import { boardmembers } from "@/data/BoardData";
@@ -16,31 +16,26 @@ const BoardWrapper = () => {
       />
 
       <div className="mx-auto grid w-5/6 place-content-center justify-center gap-x-32 sm:grid-cols-2 lg:grid-cols-3">
-      
         {boardmembers.map(
           ({ name, role, img, whyLeap, majorInfo, careerGoal }, index) => (
             <motion.div
-            initial={{ opacity: 0, y: -30 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2 , delay: index * 0.2 }}
-            whileHover={{ scale: 1.1 }}
+              initial={{ opacity: 0, y: -30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 2, delay: index * 0.2 }}
+              whileHover={{ scale: 1.1 }}
             >
-            
-              
-            <BoardMemberCards
-              name={name}
-              role={role}
-              img={img}
-              whyLeap={whyLeap}
-              majorInfo={majorInfo}
-              careerGoal={careerGoal}
-              key ={index}
-              
-            />
+              <BoardMemberCards
+                name={name}
+                role={role}
+                img={img}
+                whyLeap={whyLeap}
+                majorInfo={majorInfo}
+                careerGoal={careerGoal}
+                key={index}
+              />
             </motion.div>
           ),
         )}
-        
       </div>
     </div>
   );
