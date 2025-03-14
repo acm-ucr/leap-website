ceaafb0679716d7f0640061b1a8472c316b7f94c
qhgill <qhgill2@gmail.com>
2025-03-14 15:31:19 -0700

    fixes part 1

--- src/components/home/ValueBox.tsx
+++ src/components/home/ValueBox.tsx
@@ -9,7 +9,7 @@ const ValueBox = () => {
 
   return (
     <div className="flex min-h-[300px] items-center justify-center md:min-h-[450px] lg:min-h-[650px]">
-      <div className="grid w-full max-w-2xl grid-cols-1 gap-6 md:grid-cols-1 lg:grid-cols-2">
+      <div className="grid w-full grid-cols-1 gap-6">
         {topics.map((topic, index) => (
           <motion.div
             key={index}
@@ -17,7 +17,7 @@ const ValueBox = () => {
             whileInView={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.3, delay: 0.15 * index }}
           >
-            <div className="flex h-[150px] w-full items-center justify-center border-4 border-white bg-leap-mid-green p-6 text-center font-leap text-2xl text-white shadow-xl shadow-leap-light-green sm:h-[200px] sm:text-3xl lg:h-[300px] lg:text-4xl">
+            <div className="flex h-[10vh] w-full items-center justify-center border-4 border-white bg-leap-mid-green p-6 text-center font-leap text-2xl text-white shadow-xl shadow-leap-light-green sm:text-3xl md:h-[20vh] lg:text-4xl">
               {topic}
             </div>
           </motion.div>
--- src/components/home/ValuesContainer.tsx
+++ src/components/home/ValuesContainer.tsx
@@ -7,9 +7,9 @@ import { motion } from "motion/react";
 
 const ValuesContainer = () => {
   return (
-    <div className="md:pt-15 flex flex-row items-center justify-center space-x-8 pb-11 lg:pt-20">
+    <div className="md:pt-15 flex h-full w-full flex-row items-center justify-evenly pb-11 lg:pt-20">
       <motion.div
-        className="flex items-center justify-center md:mt-20 lg:mt-16"
+        className="flex items-center justify-evenly"
         initial={{ opacity: 0 }}
         whileInView={{ x: ["-5%", "0%"], y: ["20%", "0%"], opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.1 }}
@@ -17,32 +17,25 @@ const ValuesContainer = () => {
         <Image
           src={FlowerImg}
           alt="Flowers"
-          className="h-[300px] w-[200px] items-center md:h-[450px] md:w-[300px] lg:h-[650px] lg:w-[475px]"
+          className="h-full w-[200px] object-fill md:w-[300px] lg:w-[475px]"
         />
-        <div className="absolute translate-x-[-100%] translate-y-[-200%] md:translate-x-[-170%] md:translate-y-[-280%] lg:translate-x-[-270%] lg:translate-y-[-405%]">
-          <motion.div
-            initial={{ opacity: 0, x: -50 }}
-            whileInView={{ opacity: 1, x: 0, rotate: 360 }}
-            transition={{ duration: 0.7, delay: 0.5 }}
-          >
-            <Image
-              src={lilyPad}
-              alt="LilyPad"
-              className="h-20 w-20 rotate-180"
-            />
-          </motion.div>
-        </div>
-      </motion.div>
-
-      <div className="mr-6 mt-20 flex items-center justify-center md:mr-24 lg:mr-40 lg:mt-16">
         <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ y: ["20%", "0%"], x: ["10%", "0%"], opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.1 }}
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0, rotate: 360 }}
+          transition={{ duration: 0.7, delay: 0.5 }}
+          className="absolute translate-x-[-100%] translate-y-[-200%] md:translate-x-[-170%] md:translate-y-[-280%] lg:translate-x-[-270%] lg:translate-y-[-405%]"
         >
-          <ValueBox />
+          <Image src={lilyPad} alt="LilyPad" className="h-20 w-20 rotate-180" />
         </motion.div>
-      </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ y: ["20%", "0%"], x: ["10%", "0%"], opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="mr-6 mt-20 flex items-center justify-center md:mr-24 lg:mr-40 lg:mt-16"
+      >
+        <ValueBox />
+      </motion.div>
     </div>
   );
 };
