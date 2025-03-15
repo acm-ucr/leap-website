4592cf73255d8dd058dab5fb51d3ac1ae33ca8ae
unknown <aviambequette@gmail.com>
2025-03-15 01:20:02 -0700

    fixed month spacing, animation, background

--- src/components/events/UpcomingTitle.tsx
+++ src/components/events/UpcomingTitle.tsx
@@ -1,11 +1,21 @@
+import { motion } from "motion/react";
+
 const UpcomingTitle: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <div className="md:mt-30 relative m-5 mt-20 lg:mt-40">
+    <motion.div
+      className="md:mt-30 relative m-5 mt-20 lg:mt-40"
+      initial={{ opacity: 0, scale: 0.9 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.5,
+        delay: 0.1,
+      }}
+    >
       <p className="text-alignment mx-auto text-center font-leap text-5xl font-semibold text-leap-dark-green">
         {title}
       </p>
       <div className="relative m-2 mx-auto w-1/6 border-b-4 border-leap-mid-green p-2" />
-    </div>
+    </motion.div>
   );
 };
 
