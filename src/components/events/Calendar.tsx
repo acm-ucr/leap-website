e657a529317e9dfa5e8a1a418c2ea860b4bd1a81
Kevin Loritsch <kloritsch58@gmail.com>
2025-03-16 00:00:00 -0700

    test 2

--- src/components/events/Calendar.tsx
+++ src/components/events/Calendar.tsx
@@ -148,13 +148,7 @@ const Events = () => {
           delay: 0.1,
         }}
       >
-        {data
-          ?.filter((event: GoogleEventProps) => {
-            const eventDate = new Date(event.start.toString());
-            const currentDate = new Date();
-            return eventDate >= currentDate;
-          })
-          .slice(0, 3).length === 0 ? (
+        {data ? (
           <p className="sm:text-md mt-1 text-sm sm:mt-5 md:text-xl lg:text-2xl xl:text-3xl">
             {" "}
             No upcoming events.
@@ -162,7 +156,7 @@ const Events = () => {
         ) : (
           data
             ?.filter((event: GoogleEventProps) => {
-              const eventDate = new Date(event.start);
+              const eventDate = new Date(event.start.toString());
               const currentDate = new Date();
               return eventDate >= currentDate;
             })
