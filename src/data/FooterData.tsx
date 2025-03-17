fbba061c42761bce55dbe634cd51697bb740a868
Derrick-Mao <derrickmao03@gmail.com>
2025-03-16 19:47:55 -0700

    footer icons fix

--- src/data/FooterData.tsx
+++ src/data/FooterData.tsx
@@ -10,33 +10,25 @@ export const socials: social[] = [
   {
     link: "https://www.instagram.com/ucrleap/",
     icon: (
-      <Instagram
-        className="text-leap-light-green hover:text-leap-dark-green h-8 w-8 sm:h-10 sm:w-10"
-      />
+      <Instagram className="h-8 w-8 text-leap-light-green hover:text-leap-dark-green sm:h-10 sm:w-10" />
     ),
   },
   {
     link: "mailto:lovingouremotions@gmail.com",
     icon: (
-      <Mail
-        className="text-leap-light-green hover:text-leap-dark-green h-8 w-8 sm:h-10 sm:w-10"
-      />
+      <Mail className="h-8 w-8 text-leap-light-green hover:text-leap-dark-green sm:h-10 sm:w-10" />
     ),
   },
   {
     link: "https://linktr.ee/ucrleap",
     icon: (
-      <SiLinktree
-        className="text-leap-light-green hover:text-leap-dark-green h-8 w-8 sm:h-10 sm:w-10"
-      />
+      <SiLinktree className="h-8 w-8 text-leap-light-green hover:text-leap-dark-green sm:h-10 sm:w-10" />
     ),
   },
   {
     link: "https://docs.google.com/document/d/1quEtJKF8hjcZ77S2JVNCk2Ctr3K6JmeBMNkSkquYBsI/edit?tab=t.0",
     icon: (
-      <Newspaper
-        className="text-leap-light-green hover:text-leap-dark-green h-8 w-8 sm:h-10 sm:w-10"
-      />
+      <Newspaper className="h-8 w-8 text-leap-light-green hover:text-leap-dark-green sm:h-10 sm:w-10" />
     ),
   },
 ];
