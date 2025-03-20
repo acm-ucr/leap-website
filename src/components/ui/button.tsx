c4b4b91d73382caadaf34b0e1e17e3df1d545ce3
Kevin Loritsch <kloritsch58@gmail.com>
2025-03-19 22:16:57 -0700

    slightly better

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground active:bg-accent active:text-accent-foreground [@media(hover:none)]:hover:bg-transparent",
+          "hover:bg-accent hover:text-accent-foreground active:bg-accent active:text-accent-foreground [@media(hover:none)]:hover:bg-transparent [@media(hover:none)]:hover:accent-none",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
